a415bad755feee548a7728d1a3b1b90b83547990
Riley Grice <gricerile@myvuw.ac.nz>
2017-04-13 10:52:29 +1200

    Create motor-straight.cpp

---
+++ motor-straight.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <time.h>
+#include "E101.h"
+
+int main(){
+init();
+set_motor(1, 150);
+sleep1(3,500000);
+set_motor(2,-150);
+sleep1(3,500000);
+stop(1);
+stop(2);
+
+return(0);}
