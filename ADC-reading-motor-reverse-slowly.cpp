ace28cd7ec4676e2602aa05d999a43d7c83a71f6
Riley Grice <gricerile@myvuw.ac.nz>
2017-04-13 11:01:26 +1200

    Create ADC-reading-motor-reverse-slowly.cpp

---
+++ ADC-reading-motor-reverse-slowly.cpp
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+#include <time.h>
+#include "E101.h"
+
+int main(){
+init();
+
+while(true){
+
+Int adc_reading = read_analog(0);
+Int rotation = adc_reading / -8;
+set_motor(1.rotation);
+sleep1(0,500);
+set_motor(2.-rotation);
+sleep1(0,500);
+}
+
+Return 0;}
