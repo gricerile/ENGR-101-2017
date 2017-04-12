154d5e92e0d4cef5ea0d7166c9fbe32602bf1c25
Riley Grice <gricerile@myvuw.ac.nz>
2017-04-13 10:53:47 +1200

    Create ADC-reading-motor.cpp

---
+++ ADC-reading-motor.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <time.h>
+#include "E101.h"
+
+int main(){
+init();
+
+while(true){
+
+Int adc_reading = read_analog(0);
+Int rotation = adc_reading / 4;
+set_motor(1.rotation);
+sleep1(0,500);
+}
+
+Return 0;}
