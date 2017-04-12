5e9daf6dc5370733be217c2a1ac6580766180270
Riley Grice <gricerile@myvuw.ac.nz>
2017-04-13 10:59:55 +1200

    Create motor-reverse-slowly.cpp

---
+++ motor-reverse-slowly.cpp
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+#include <time.h>
+#include "E101.h"
+
+int main(){
+init();
+set_motor(1, -50);
+sleep1(3,500000);
+set_motor(2,50);
+sleep1(3,500000);
+stop(1);
+stop(2);
+
+return(0);}
